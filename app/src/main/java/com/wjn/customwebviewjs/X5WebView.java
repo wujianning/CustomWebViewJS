980412925ba1af940acbbd62b7f506cd38c73933
wujianning <tmac324421@126.com>
2019-03-18 16:01:28 +0800

    update

---
+++ app/src/main/java/com/wjn/customwebviewjs/X5WebView.java
@@ -0,0 +1,113 @@
+package com.wjn.customwebviewjs;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tencent.smtt.export.external.interfaces.WebResourceRequest;
+import com.tencent.smtt.export.external.interfaces.WebResourceResponse;
+import com.tencent.smtt.sdk.CookieManager;
+import com.tencent.smtt.sdk.CookieSyncManager;
+import com.tencent.smtt.sdk.DownloadListener;
+import com.tencent.smtt.sdk.WebChromeClient;
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class X5WebView extends WebView {
+
+    private static final int MAX_LENGTH = 8;
+    private List<String> newList;
+
+    /**
+     * 构造方法
+     * */
+
+    public X5WebView(Context context) {
+        super(context);
+        initUI();
+    }
+
+    /**
+     * 构造方法
+     * */
+
+    public X5WebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initUI();
+    }
+
+    /**
+     * 构造方法
+     * */
+
+    public X5WebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initUI();
+    }
+
+    /**
+     * 初始化View
+     * */
+
+    private void initUI() {
+        setHorizontalScrollBarEnabled(false);//水平不显示小方块
+        setVerticalScrollBarEnabled(false); //垂直不显示小方块
+//      setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);//滚动条在WebView内侧显示
+//      setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);//滚动条在WebView外侧显示
+        initWebViewSettings();
+    }
+
+    /**
+     * 初始化WebViewSetting
+     * */
+
+    public void initWebViewSettings() {
+        setBackgroundColor(getResources().getColor(android.R.color.white));
+        setClickable(true);
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+        WebSettings webSetting = getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setAllowFileAccess(true);
+        webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSetting.setSupportZoom(true);
+        webSetting.setUseWideViewPort(true);
+        webSetting.setSupportMultipleWindows(true);
+        webSetting.setAppCacheEnabled(true);
+        webSetting.setGeolocationEnabled(true);
+        webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
+        webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
+        webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        //android 默认是可以打开_bank的，是因为它默认设置了WebSettings.setSupportMultipleWindows(false)
+        //在false状态下，_bank也会在当前页面打开……
+        //而x5浏览器，默认开启了WebSettings.setSupportMultipleWindows(true)，
+        // 所以打不开……主动设置成false就可以打开了
+        //需要支持多窗体还需要重写WebChromeClient.onCreateWindow
+        webSetting.setSupportMultipleWindows(false);
+    }
+
+}
--- app/src/main/java/com/wjn/customwebviewjs/X5WebViewActivity.java
+++ app/src/main/java/com/wjn/customwebviewjs/X5WebViewActivity.java
@@ -2,12 +2,114 @@ package com.wjn.customwebviewjs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.tencent.smtt.sdk.WebChromeClient;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
 
 public class X5WebViewActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private X5WebView x5WebView;
+    private String url = "https://blog.csdn.net/weixin_37730482";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_x5webview);
+        initHardwareAccelerate();
+        initView();
     }
+
+    /**
+     * 启用硬件加速
+     */
+
+    private void initHardwareAccelerate() {
+        try {
+            if (Integer.parseInt(android.os.Build.VERSION.SDK) >= 11) {
+                getWindow()
+                        .setFlags(
+                                android.view.WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                                android.view.WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * 初始化各种View
+     * */
+
+    private void initView() {
+        progressBar=findViewById(R.id.activity_x5webview_progressbar);
+        x5WebView = findViewById(R.id.activity_x5webview_x5webview);
+        x5WebView.loadUrl(url);
+        x5WebView.setWebViewClient(new myWebViewClient());
+        x5WebView.setWebChromeClient(new myWebChromeClient());
+    }
+
+    /**
+     * WebViewClient监听
+     * */
+
+    private class myWebViewClient extends WebViewClient{
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String s) {
+            webView.loadUrl(s);
+            return true;
+        }
+    }
+
+    /**
+     * WebChromeClient监听
+     * */
+
+    private class myWebChromeClient extends WebChromeClient{
+
+        @Override
+        public void onProgressChanged(WebView webView, int i) {
+            super.onProgressChanged(webView, i);
+            if (i == 100) {
+                progressBar.setVisibility(View.GONE);//加载完网页进度条消失
+            } else {
+                progressBar.setVisibility(View.VISIBLE);//开始加载网页时显示进度条
+                progressBar.setProgress(i);//设置进度值
+            }
+        }
+    }
+
+    /**
+     * onKeyDown方法
+     */
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (x5WebView != null && x5WebView.canGoBack()) {
+                x5WebView.goBack();
+                return true;
+            } else {
+                return super.onKeyDown(keyCode, event);
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * onDestroy方法
+     * */
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(null!=x5WebView){
+            x5WebView.destroy();//释放资源
+        }
+    }
+
 }
